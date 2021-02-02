c5dd886cae573c45914a9273bbbb2a885397a21f
suleesulee <56288518+suleesulee@users.noreply.github.com>
2021-02-02 21:18:14 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import GetOldTweets3 as GetOldTweets3
+import datetime
+
+Date = []
+
+start = datetime.datetime.strptime("2020-11-30", "%Y-%M-%D") 
+end = datetime.datetime.strptime("2021-11-30", "%Y-%M-%D")  #원하는 날짜
